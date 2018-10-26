f4fe381c58e6b0856b56e906baa10ee3a3580665
Fabio <fabiozoroastro@gmail.com>
2018-10-26 05:27:36 -0700

    setup kotlin,springboot app

---
+++ src/main/kotlin/zoret4/payme/PaymeApplication.kt
@@ -0,0 +1,11 @@
+package zoret4.payme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymeApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymeApplication>(*args)
+}
